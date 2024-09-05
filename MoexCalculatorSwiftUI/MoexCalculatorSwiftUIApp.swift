7e29e1fb79d5f4ae901252d4a4c187fe32766792
Giya077 <giyaDevIos@gmail.com>
2024-09-05 12:29:33 +0500

    Initial Commit

---
+++ MoexCalculatorSwiftUI/MoexCalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorSwiftUIApp.swift
+//  MoexCalculatorSwiftUI
+//
+//  Created by GiyaDev on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
